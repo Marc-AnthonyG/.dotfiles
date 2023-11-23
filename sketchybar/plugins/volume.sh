20085600ff476756bcd7f891d67536df39f0f335
Marc-Anthony Girard <marc-anthonygirard@Marc-Anthonys-MacBook-Pro.local>
2023-11-23 16:28:52 -0500

    mac lol

--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -7,13 +7,11 @@ if [ "$SENDER" = "volume_change" ]; then
   VOLUME=$INFO
 
   case $VOLUME in
-    [6-9][0-9]|100) ICON="墳"
+    [5-9][0-9]|100) ICON="󰕾"
     ;;
-    [3-5][0-9]) ICON="奔"
+    [1-9]|[1-4][0-9]) ICON="󰖀"
     ;;
-    [1-9]|[1-2][0-9]) ICON="奄"
-    ;;
-    *) ICON="婢"
+    *) ICON="󰖁"
   esac
 
   sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
