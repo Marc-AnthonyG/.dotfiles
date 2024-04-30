05321dd29c8bc8be2adb450e673bb79ee1fabe7f
Marc-AnthonyG <marcanthony.girard.pro@gmail.com>
2024-04-30 11:01:18 -0400

    change to rofi powermenu

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ This repository is my config. All the config files are listed here. Doing sh ins
 # Todo 
 - [ ] Beatify polybar
 - [ ] Beatify rofi
-- [ ] Take a look at Thunar for file manager 
 - [ ] Take a look at Dunst for notification
--- rofi/powermenu.sh
+++ rofi/powermenu.sh
@@ -4,21 +4,18 @@ if [ -z "$@" ]; then
     echo -en "Logout\0icon\x1fsystem-log-out\n"
     echo -en "Shutdown\0icon\x1fsystem-shutdown\n"
     echo -en "Suspend\0icon\x1fsystem-suspend\n"
-    echo -en "Hibernate\0icon\x1fsystem-suspend-hibernate\n"
     echo -en "Reboot\0icon\x1fsystem-reboot\n"
     echo -en "Lock\0icon\x1fsystem-lock-screen\n"
 else
     if [ "$1" = "Shutdown" ]; then
-        sudo shutdown 0 
+        shutdown now
     elif [ "$1" = "Logout" ]; then
         i3 exit logout
     elif [ "$1" = "Reboot" ]; then
-        sudo shtudown -r 
+        reboot 
     elif [ "$1" = "Suspend" ]; then
         systemctl suspend
     elif [ "$1" = "Lock" ]; then
-        i3exit lock
-    elif [ "$1" = "Hibernate" ]; then
-        sudo systemctl hibernate
+        i3lock
     fi
 fi
