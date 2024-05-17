75eea8b07da07f5411a46f2416abf9d64ce89ec3
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2024-05-17 08:56:11 -0400

    some ajustement

--- sketchybar/plugins/gitNotifications.sh
+++ sketchybar/plugins/gitNotifications.sh
@@ -3,15 +3,17 @@
 NOTIFICATIONS="$(gh api notifications)"
 COUNT="$(echo "$NOTIFICATIONS" | jq 'length')"
 args=()
-if [ "$NOTIFICATIONS" = "[]" ]; then
+if [ "$COUNT" = "0" ]; then
   args+=(--set $NAME icon=􀋚 label="0")
 else
   args+=(--set $NAME icon=􀝗 label="$COUNT")
 fi
 
-args+=(--remove '/github.notification\.*/')
+#args+=(--remove '/github.notification\.*/')
 
 COUNT=0
+COLOR=0xffe1e3e4
+args+=(--set github.bell icon.color=$COLOR)
 
 while read -r repo url type title 
 do
@@ -20,11 +22,25 @@ do
   COLOR=0xff72cce8
   PADDING=0
   case "${type}" in
-    "'Issue'") COLOR=0xff9dd274; ICON=􀍷; PADDING=0; URL="$(gh api "$(echo "${url}" | sed -e "s/^'//" -e "s/'$//")" | jq .html_url)"
+    "'Issue'") 
+      COLOR=0xff9dd274; 
+      ICON=􀍷; 
+      PADDING=0;
+      URL="$(gh api "$(echo "${url}" | sed -e "s/^'//" -e "s/'$//")" | jq .html_url)"
+      echo $URL
     ;;
-    "'Discussion'") COLOR=0xffe1e3e4; ICON=􀒤; PADDING=0; URL="https://www.github.com/notifications"
+    "'Discussion'") 
+      COLOR=0xffe1e3e4; 
+      ICON=􀒤; 
+      PADDING=0; 
+      URL="https://www.github.com/notifications"
     ;;
-    "'PullRequest'") COLOR=0xffba9cf3; ICON="􀙡"; PADDING=4; URL="$(gh api "$(echo "${url}" | sed -e "s/^'//" -e "s/'$//")" | jq .html_url)"
+    "'PullRequest'") 
+      COLOR=0xffba9cf3; 
+      ICON="􀙡"; 
+      PADDING=4;
+      URL="$(gh api "$(echo "${url}" | sed -e "s/^'//" -e "s/'$//")" | jq .html_url)"
+      echo $URL
     ;;
   esac
   
@@ -43,7 +59,8 @@ do
                         ;;
                         esac' \
                click_script="open $URL;
-                             sketchybar --set github.bell popup.drawing=toggle"
+                             sketchybar --set github.bell popup.drawing=toggle
+                             "
 --subscribe github.notification.$COUNT mouse.entered mouse.exited)
 
 done <<< "$(echo "$NOTIFICATIONS" | jq -r '.[] | [.repository.name, .subject.latest_comment_url, .subject.type, .subject.title] | @sh')"
--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -1,35 +1,21 @@
 #!/bin/sh
 
-source "$HOME/.config/sketchybar/plugins/icons.sh"
-
-# The $SELECTED variable is available for space components and indicates if
-# the space invoking this script (with name: $NAME) is currently selected:
-# https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
-
 sketchybar --set $NAME background.drawing=$SELECTED 
 
 if [[ $SENDER == "window_change" || $SENDER == "space_change" ]]; then
- for sid in {1..6}
- do
+   sid="${NAME: -1}"
    LABEL=""
-   QUERY=$(yabai -m query --windows --space $sid)
-   APPS=$(echo $QUERY | jq '.[].app')
-   TITLES=$(echo $QUERY | jq '.[].title')
+   APPS=$(echo $(yabai -m query --windows --space $sid)| jq '.[].app')
  
-   if grep -q "\"" <<< $APPS;
-   then
+   if [[ -n "$APPS" ]]; then
      APPS_ARR=()
-     while read -r line; do APPS_ARR+=("$line"); done <<< "$APPS"
-     TITLES_ARR=()
-     while read -r line; do TITLES_ARR+=("$line"); done <<< "$TITLES"
+     while read -r line; do APPS_ARR+=("${line}"); done <<< "$APPS"
  
      LENGTH=${#APPS_ARR[@]}
-     for j in "${!APPS_ARR[@]}"
-     do
+
+     for j in "${!APPS_ARR[@]}"; do
        APP=$(echo ${APPS_ARR[j]} | sed 's/"//g')
-       TITLE=$(echo ${TITLES_ARR[j]} | sed 's/"//g')
- 
-       ICON=$($HOME/.config/sketchybar/plugins/icons.sh "$APP" "$TITLE")
+       ICON=$($HOME/.config/sketchybar/plugins/icons.sh "$APP")
        LABEL+="$ICON"
        if [[ $j < $(($LENGTH-1)) ]]; then
          LABEL+=" "
@@ -40,5 +26,4 @@ if [[ $SENDER == "window_change" || $SENDER == "space_change" ]]; then
    fi
  
    sketchybar --set space.$sid label="$LABEL"
- done
 fi
