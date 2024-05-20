3c9e76670115ce03709b3e15d57b9185c3c7a32f
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2024-05-20 11:18:09 -0400

    correct small bug in icon selection

--- sketchybar/items/spaces.lua
+++ sketchybar/items/spaces.lua
@@ -8,27 +8,19 @@ local white = TO_FULL_COLORS(colors.white, 100)
 local selected_color = TO_FULL_COLORS(colors.blue.sky, 100)
 
 local function set_label_as_app_icon(item, SID)
-  SBAR.exec("yabai -m query --windows --space " .. SID .. "| jq '.[].app'", function(apps)
-    local label = ""
+  SBAR.exec("yabai -m query --windows --space " .. SID .. "|  jq '[.[] | .app]'", function(apps)
+    local label = "_"
+    if #apps > 0 then
+      label = ""
 
-    if apps and apps ~= "" then
-      local apps_arr = {}
-      for line in apps:gmatch("[^\r\n]+") do
-        local app_name = line:gsub('"', '')
-        table.insert(apps_arr, app_name)
-      end
-
-      local length = #apps_arr
-
-      for j, app_name in ipairs(apps_arr) do
+      local length = #apps
+      for j, app_name in ipairs(apps) do
         local icon = get_icon(app_name)
         label = label .. icon
         if j < length then
           label = label .. " "
         end
       end
-    else
-      label = "_"
     end
 
     item:set({ label = label })
@@ -38,6 +30,7 @@ end
 local function space_selection(env)
   local sid_num = tonumber(env.SID)
   local item = spaces[sid_num]
+
   if env.SELECTED == "true" then
     item:set({
       background = { drawing = "on" },
@@ -53,27 +46,6 @@ local function space_selection(env)
   end
 
   set_label_as_app_icon(item, env.SID)
-end
-
-local function on_hover(env)
-  local item = spaces[tonumber(env.SID)]
-
-  if env.SENDER == "mouse.entered" then
-    item:set({
-      icon = { color = colors.hover_color },
-      label = { color = colors.hover_color },
-      background = { drawing = "on", color = TO_FULL_COLORS(colors.night.lighter, 80) },
-    })
-  elseif env.SENDER == "mouse.exited" then
-    item:set({
-      icon = { color = env.SELECTED == "true" and selected_color or white },
-      label = { color = env.SELECTED == "true" and selected_color or white },
-      background = {
-        drawing = env.SELECTED == "true" and "on" or "off",
-        color = TO_FULL_COLORS(colors.white, 26),
-      },
-    })
-  end
 end
 
 
@@ -95,7 +67,6 @@ for sid = 1, 8, 1 do
   })
 
   spaces[sid] = space
-  space:subscribe("mouse.entered", on_hover)
-  space:subscribe("mouse.exited", on_hover)
   space:subscribe("space_change", space_selection)
+  space:subscribe("window_change", space_selection)
 end
