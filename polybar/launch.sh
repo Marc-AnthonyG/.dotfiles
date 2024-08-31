d06f1c2744eb584739461ebbcf10bb5a850683cd
Marc-AnthonyG <marcanthony.girard.pro@gmail.com>
2024-08-30 21:37:43 -0400

    feat: small change to polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,6 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar side 2>&1 | tee -a /tmp/polybar2.log & disown
+#polybar side 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "polybar succesfully launch"
