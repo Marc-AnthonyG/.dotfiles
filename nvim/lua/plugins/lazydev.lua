a7042500af1b62b8cab7565f9ec79364a3544d56
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2024-11-03 14:22:11 -0500

    feat: remove blink

--- nvim/lua/plugins/lazydev.lua
+++ nvim/lua/plugins/lazydev.lua
@@ -20,21 +20,5 @@ return {
 				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
 			})
 		end,
-	},
-	{ -- optional blink completion source for require statements and module annotations
-		"saghen/blink.cmp",
-		opts = {
-			sources = {
-				-- add lazydev to your completion providers
-				completion = {
-					enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
-				},
-				providers = {
-					-- dont show LuaLS require statements when lazydev has items
-					lsp = { fallback_for = { "lazydev" } },
-					lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
-				},
-			},
-		},
 	}
 }
