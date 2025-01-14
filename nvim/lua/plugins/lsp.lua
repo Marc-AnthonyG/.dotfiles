d19b88705056c4a0e4c13882a7dd084d5780c40f
Marc-Anthony Girard <marcanthony.girard.pro@gmail.com>
2025-01-13 22:17:52 -0500

    feat: reput setup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -79,6 +79,15 @@ return {
 					},
 				},
 			},
+			setup = {
+				-- example to setup with typescript.nvim
+				-- tsserver = function(_, opts)
+				--   require("typescript").setup({ server = opts })
+				--   return true
+				-- end,
+				-- Specify * to use this function as a fallback for any server
+				-- ["*"] = function(server, opts) end,
+			},
 		}
 		return ret
 	end,
