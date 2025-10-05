e455820ded0c7767f437746512d2e7ffcb53d66c
Marc-AnthonyG <marcanthony.girard.pro@gmail.com>
2025-10-05 11:27:13 -0400

    feat: fix ai copilot

--- common/nvim/lua/plugins/ai.lua
+++ common/nvim/lua/plugins/ai.lua
@@ -1,6 +1,68 @@
 return {
 	{
+		'zbirenbaum/copilot.lua',
+		event = { 'InsertEnter' },
+		cmd = 'Copilot',
+		opts = {
+			panel = {
+				enabled = true,
+				auto_refresh = false,
+				keymap = {
+					jump_prev = '[[',
+					jump_next = ']]',
+					accept = '<CR>',
+					refresh = 'gr',
+					open = '<M-CR>',
+				},
+				layout = {
+					position = 'bottom', -- | top | left | right | bottom |
+					ratio = 0.4,
+				},
+			},
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				hide_during_completion = false,
+				debounce = 75,
+				trigger_on_accept = true,
+				keymap = {
+					accept = '<tab>',
+					accept_word = false,
+					accept_line = false,
+					next = '<M-]>',
+					prev = '<M-[>',
+					dismiss = '<C-]>',
+				},
+			},
+			root_dir = function()
+				return vim.fs.dirname(vim.fs.find('.git', { upward = true })[1])
+			end,
+			should_attach = function(_, _)
+				if not vim.bo.buflisted then
+					Util.log.debug("not attaching, buffer is not 'buflisted'")
+					return false
+				end
+
+				if vim.bo.buftype ~= '' then
+					Util.log.debug("not attaching, buffer 'buftype' is " .. vim.bo.buftype)
+					return false
+				end
+
+				Util.log.debug('Copilot attaching to current buffer.')
+				return true
+			end,
+			server = {
+				type = 'nodejs', -- "nodejs" | "binary"
+				custom_server_filepath = nil,
+			},
+			server_opts_overrides = {},
+		},
+	},
+	{
 		'folke/sidekick.nvim',
+		dependencies = {
+			'zbirenbaum/copilot.lua',
+		},
 		opts = function()
 			-- Accept inline suggestions or next edits
 			Util.cmp.actions.ai_nes = function()
